b1783505f7908aaa3ea5cf9b8efe6b7663aa7594
ToasterLightning <toasterlightning@gmail.com>
2025-02-10 23:32:43 -0800

    water particles now affected by gravity

--- particlesim.py
+++ particlesim.py
@@ -10,17 +10,24 @@ def summonWater(x,y, waterParticles):
             theta = random.uniform(0,2*math.pi)
             posX = r*math.cos(theta) + x
             posY = r*math.sin(theta) + y
-        waterParticles.append((posX,posY))
+        waterParticles.append([posX,posY, 0])
     return waterParticles
 def drawWater(waterParticles, screen):
     for p in waterParticles:
-        pygame.draw.circle(screen, (0, 120, 255), p, 1)
+        pygame.draw.circle(screen, (0, 120, 255), (p[0], p[1]), 1)
+def physicsUpdate(waterParticles):
+    for p in waterParticles:
+        p[2] += .1
+        p[1] += p[2]
+    return waterParticles
 
 waterParticles = []
 
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Particle Sim")
+
+clock = pygame.time.Clock()
 
 running = True
 while running:
@@ -29,7 +36,10 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
             summonWater(*event.pos,waterParticles)
+            print(event.pos)
     screen.fill((255,255,255))
     drawWater(waterParticles,screen)
     pygame.display.flip()
+    waterParticles = physicsUpdate(waterParticles)
+    clock.tick(60)
 pygame.quit()
