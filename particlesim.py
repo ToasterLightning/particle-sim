8a865dad6d27ffe659f0dae5c786a9ae674df27f
ToasterLightning <toasterlightning@gmail.com>
2025-02-10 23:20:01 -0800

    ok we can now make particles appear on command

---
+++ particlesim.py
@@ -0,0 +1,35 @@
+import pygame
+import random
+import math
+def summonWater(x,y, waterParticles):
+    for i in range(100):
+        posX = -500
+        posY = -500
+        while (not 0 < posX < 600) and (not 0 < posY < 600):
+            r = math.sqrt(random.random())*25
+            theta = random.uniform(0,2*math.pi)
+            posX = r*math.cos(theta) + x
+            posY = r*math.sin(theta) + y
+        waterParticles.append((posX,posY))
+    return waterParticles
+def drawWater(waterParticles, screen):
+    for p in waterParticles:
+        pygame.draw.circle(screen, (0, 120, 255), p, 1)
+
+waterParticles = []
+
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption("Particle Sim")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+            summonWater(*event.pos,waterParticles)
+    screen.fill((255,255,255))
+    drawWater(waterParticles,screen)
+    pygame.display.flip()
+pygame.quit()
